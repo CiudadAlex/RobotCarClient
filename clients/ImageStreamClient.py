ee300cd97ea2eaddc0041315ad7fcab41c519d06
CiudadAlex <ciudad.alex@gmail.com>
2024-10-16 17:13:22 +0200

    RobotCarClient

---
+++ clients/ImageStreamClient.py
@@ -0,0 +1,26 @@
+from clients.AbstractStreamClient import AbstractStreamClient
+from PIL import Image
+
+
+class ImageStreamClient(AbstractStreamClient):
+
+    def __init__(self, host, port):
+        super().__init__(host, port)
+        self.count = 0
+        self.start()
+
+    def use_item_metadata_and_bytes(self, item_metadata, item_bytes):
+
+        str_item_metadata = item_metadata.decode("utf-8")
+        array_metadata = str_item_metadata.split(",")
+        width = int(array_metadata[0])
+        height = int(array_metadata[1])
+        image = Image.frombytes('RGBA', (width, height), item_bytes)
+
+        if self.count % 30 == 0:
+            image.show()
+
+        self.count = self.count + 1
+
+# FIXME finish
+
--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-import socket
-import struct
-from PIL import Image
+import time
 
-
-def receive_images():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('192.168.0.19', 8000))
-
-    while True:
-        image_size = struct.unpack('>I', client_socket.recv(4))[0]
-        print("image_size = " + str(image_size))
-        image_bytes = b""
-        while len(image_bytes) < image_size:
-            packet = client_socket.recv(4096)
-            if not packet:
-                break
-            image_bytes += packet
-
-        image = Image.frombytes('RGBA', (240, 60), image_bytes)
-        image.show()
+from clients.ImageStreamClient import ImageStreamClient
 
 
 if __name__ == "__main__":
-    receive_images()
+    imageStreamClient = ImageStreamClient('192.168.0.19', 8000)
+
+    time.sleep(30)
+
 
 # FIXME check image dimensions
 # FIXME finish
