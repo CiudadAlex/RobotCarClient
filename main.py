f9cd7556c77de3bc0a8201460227fc963de39075
CiudadAlex <ciudad.alex@gmail.com>
2024-11-07 23:38:35 +0100

    RobotCarClient

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from remotecontrolui.RemoteControlUI import RemoteControlUI
 
 if __name__ == "__main__":
 
-    RemoteControlUI.launch(connect_to_video_stream=False, connect_to_audio_or_text_command_stream=False)
+    RemoteControlUI.launch(connect_to_video_stream=True, connect_to_audio_or_text_command_stream=True)
 
-# FIXME test and optimize
+# FIXME problem with speech detection
 
