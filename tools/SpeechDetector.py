0f5a487de82131640b93f1bdf5df55706119d365
CiudadAlex <ciudad.alex@gmail.com>
2024-11-08 00:12:49 +0100

    RobotCarClient

--- tools/SpeechDetector.py
+++ tools/SpeechDetector.py
@@ -1,14 +1,56 @@
+import time
 import webrtcvad
+import numpy as np
+import wave
+import pyaudio
+import matplotlib.pyplot as plt
 
 
 class SpeechDetector:
 
-    def __init__(self, aggressiveness=3):
+    def __init__(self, aggressiveness=0, debug=True):
         self.vad = webrtcvad.Vad(aggressiveness)
+        self.debug = debug
+        self.chans = 1
+        self.rate = 16000
+        self.audio_format = pyaudio.paInt16
+
+        self.p = pyaudio.PyAudio()
+        self.idx = 0
 
     @staticmethod
     def divide_bytearray(byte_array, piece_size):
         return [byte_array[i:i + piece_size] for i in range(0, len(byte_array), piece_size)]
+
+    def energy(self, chunk):
+        # Calculates the energy of the chunk
+        np_array = np.frombuffer(chunk, dtype=np.int16)
+        energy = np.sum(np.abs(np_array))
+
+        # self.store_audio_file(chunk)
+
+        plt.plot(np_array)
+        plt.show()
+        time.sleep(10)
+
+        if self.debug:
+            print(f"Energy = {energy}")
+
+        return energy
+
+    def store_audio_file(self, audio_chunk):
+
+        self.idx = self.idx + 1
+
+        # creates wave file with audio read in
+        # Code is from the wave file audio tutorial as referenced below
+        wav_output_filename = f'test{self.idx}.wav'
+        wavefile = wave.open(wav_output_filename, 'wb')
+        wavefile.setnchannels(self.chans)
+        wavefile.setsampwidth(self.p.get_sample_size(self.audio_format))
+        wavefile.setframerate(self.rate)
+        wavefile.writeframes(audio_chunk)
+        wavefile.close()
 
     def detect_voice(self, audio_chunk):
 
@@ -19,6 +61,7 @@ class SpeechDetector:
         piece_size = int(bytes_per_sample * sample_rate * frame_duration)
 
         list_frames = self.divide_bytearray(audio_chunk, piece_size)
+        #  self.energy(audio_chunk)
 
         # Evaluate each frame to detect speech
         is_speech_detected = False
