eb254f9a70e8de2f6afba951a8b17452813adf9d
CiudadAlex <ciudad.alex@gmail.com>
2024-12-09 14:19:24 +0100

    RobotCarClient

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ if __name__ == "__main__":
 
     RemoteControlUI.launch(connect_to_video_stream=True, connect_to_audio_or_text_command_stream=True)
 
-# FIXME Command: Follow me
-# FIXME Command: Go to room
+# FIXME test Questions to wikipedia API
+# FIXME test play music, answer wikipedia, plain command
+
+
+# FIXME build Car itself
+# FIXME Command: Follow me (use Tracker)
 # FIXME Command: Photos 360
 # FIXME Generator Corpus Yolo
+
+
+# FIXME LLM with contexts
+# FIXME Command: Go to room
 # FIXME Door recognizer
 # FIXME Record Video
-# FIXME LLM with contexts (play music, answer wikipedia, plain command)
 # FIXME Speakers to answer
-# FIXME Questions to wikipedia API
+
 
