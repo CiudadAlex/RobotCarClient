46a4533b9c5330105462c5a12fe3e0760b592334
CiudadAlex <ciudad.alex@gmail.com>
2024-12-10 11:19:33 +0100

    RobotCarClient

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
 
     RemoteControlUI.launch(connect_to_video_stream=True, connect_to_audio_or_text_command_stream=True)
 
-# FIXME test Questions to wikipedia API
-# FIXME test play music, answer wikipedia, plain command
+# FIXME switch off listening while music o voice playing
+# FIXME test ask LLM
 
 
 # FIXME Command: Follow me (use Tracker)
