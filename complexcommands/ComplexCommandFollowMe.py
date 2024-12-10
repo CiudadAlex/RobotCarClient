e1282280d198fcbf3c73a1308304389d157eb22d
CiudadAlex <ciudad.alex@gmail.com>
2024-12-10 14:49:00 +0100

    RobotCarClient

--- complexcommands/ComplexCommandFollowMe.py
+++ complexcommands/ComplexCommandFollowMe.py
@@ -23,14 +23,20 @@ class ComplexCommandFollowMe:
         self.commands_client = CommandsClient.get_instance()
         self.object_detector = ObjectDetector.load_standard_model("s")
         self.car_movement = CarMovement()
-        self.tracker = Tracker(width, height, self.car_movement)
+        self.tracker = Tracker(width=320, height=240, car_movement=self.car_movement)
 
     def execute(self):
 
         self.running = True
 
-        if not self.running:
-            return
+        while self.running:
+
+            results = self.object_detector.predict(self.last_image)
+            point1, point2 = self.object_detector.get_bounding_box_vertices_of_single_object_of_class(results, "person")
+
+            rectangle_up_left_position = point1
+            rectangle_down_right_position = point2
+            self.tracker.track(rectangle_up_left_position, rectangle_down_right_position)
 
     # FIXME implement
 
