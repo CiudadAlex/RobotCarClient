f00f3bf1b8ccad11da484c5edea8598ad1d0b927
CiudadAlex <ciudad.alex@gmail.com>
2024-12-30 20:28:23 +0100

    RobotCarClient

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from remotecontrolui.RemoteControlUI import RemoteControlUI
 from engine.Engine import Engine
 
 
+online = True
+
+
 if __name__ == "__main__":
     Engine.start(car_speaks=False,
                  commands_by_audio=True,
-                 connect_to_video_stream=True,
-                 connect_to_audio_or_text_command_stream=True)
+                 connect_to_video_stream=online,
+                 connect_to_audio_or_text_command_stream=online)
 
     RemoteControlUI.launch()
 
